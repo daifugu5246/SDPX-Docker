b916843000e49507d76871bf6a77c36d09aec849
daifugu5246 <patipat.kd@gmail.com>
2024-01-09 02:10:52 +0700

    fix : change image name and add user for DB

--- api/app.py
+++ api/app.py
@@ -3,8 +3,8 @@ from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_ROOT_PASSWORD')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DATABASE')
 
 mysql = MySQL(app)
